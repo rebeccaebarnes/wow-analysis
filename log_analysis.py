378990958461007046f11c3ea596b40b04315661
Rebecca Barnes <rebeccaebarnes@gmail.com>
2018-07-06 19:42:15 -0700

    fix: Add metric by role info to max_parses

--- log_analysis.py
+++ log_analysis.py
@@ -1,10 +1,19 @@
 def max_parses(df, player_name, primary_role):
-    # Create df of only player and primary role info
-    player_df = df[(df.player_name == player_name) & (df.primary_role == primary_role)]
+    # Confirm metric
+    if primary_role == 'damage':
+        metric = 'dps'
+    if primary_role == 'healer':
+        metric = 'hps'
+    if primary_role == 'tank':
+        metric = 'tankhps'
+
+    # Create df of only player and primary role/metric info
+    player_df = df[(df.player_name == player_name) & (df.metric == metric)]
     index_list = []
+
     # Find rows with max parses
     for boss in player_df.boss_name:
-        boss_rows = player_df[player_df.boss_name == boss]
+        boss_rows = player_df[(player_df.boss_name == boss)]
         # Manage bosses with rankings for multiple specs
         if boss_rows.shape[0] > 1:
             index = boss_rows.percentile.idxmax()
