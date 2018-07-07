98e758618cd0e8320d099507c2bbef2a7e011856
Rebecca Barnes <rebeccaebarnes@gmail.com>
2018-07-06 18:10:58 -0700

    fix: Add correct pathing to extract_fights

--- wow_analysis.py
+++ wow_analysis.py
@@ -111,8 +111,9 @@ def extract_fights():
                                      'player_name'])
 
     # Read through all the files and create a fight info summary df
-    for file in os.listdir('log_details'):
-        with open(file) as json_file:
+    folder_name = 'log_details'
+    for file in os.listdir(folder_name):
+        with open(os.path.join(folder_name, file)) as json_file:
             data = json.load(json_file)
 
         # Collect fight info
